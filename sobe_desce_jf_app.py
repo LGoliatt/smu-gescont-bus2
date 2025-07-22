82b31d9f486cb59ac82d30726d21f96288750cfb
LGoliatt <goliatt@gmail.com>
2025-07-22 15:27:32 -0300

    goliatt ter 22 jul 2025 15:27:32 -03

--- sobe_desce_jf_app.py
+++ sobe_desce_jf_app.py
@@ -95,8 +95,8 @@ route_options = file_list
 selected_routes = st.sidebar.multiselect(
     "Select Route(s):",
     options=route_options,
-    #default=[route_options[0]] if len(route_options) > 0 else None
-    default=['100', '101', '102', '103', '104', '105',]
+    default=[route_options[0]] if len(route_options) > 0 else None
+    #default=['100', '101', '102', '103', '104', '105',]
 )
 
 # Time selector for the selected route
